3f02d6f35ee7602ee784ff326f42dedaa4021c57
Ashish Sharda <ashishjsharda@gmail.com>
2023-02-12 06:22:49 -0600

    add persistence dependency

--- src/main/java/com/example/hateoasdemo/Employee.java
+++ src/main/java/com/example/hateoasdemo/Employee.java
@@ -1,5 +1,8 @@
 package com.example.hateoasdemo;
-
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+@Entity
 public class Employee {
 
         private  String name;
