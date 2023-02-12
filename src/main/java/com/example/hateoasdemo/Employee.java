758d7171be49b2ee87fc0866f89063f1a29ec658
Ashish Sharda <ashishjsharda@gmail.com>
2023-02-12 06:26:12 -0600

    add getter and setter

--- src/main/java/com/example/hateoasdemo/Employee.java
+++ src/main/java/com/example/hateoasdemo/Employee.java
@@ -5,9 +5,33 @@ import javax.persistence.Id;
 @Entity
 public class Employee {
 
+        public String getName() {
+                return name;
+        }
+
+        public void setName(String name) {
+                this.name = name;
+        }
+
+        public String getCompany() {
+                return company;
+        }
+
+        public void setCompany(String company) {
+                this.company = company;
+        }
+
+        public Long getId() {
+                return id;
+        }
+
+        public void setId(Long id) {
+                this.id = id;
+        }
+
         private  String name;
         private String company;
-        private long id;
+        private @Id @GeneratedValue Long id;
 
 
 }
